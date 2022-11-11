a568b9f975f1ded2a9b459004aa9292bd1016934
quarkusio <no-reply@quarkus.io>
2022-11-11 20:58:18 +0000

    Initial commit

---
+++ src/main/kotlin/com/dmartinc/MyLivenessCheck.kt
@@ -0,0 +1,14 @@
+package com.dmartinc
+
+import org.eclipse.microprofile.health.HealthCheck
+import org.eclipse.microprofile.health.HealthCheckResponse
+import org.eclipse.microprofile.health.Liveness
+
+@Liveness
+public class MyLivenessCheck : HealthCheck {
+
+    override fun call(): HealthCheckResponse {
+        return HealthCheckResponse.up("alive")
+    }
+
+}
---
+++ src/native-test/kotlin/com/dmartinc/GreetingResourceIT.kt
@@ -0,0 +1,6 @@
+package com.dmartinc
+
+import io.quarkus.test.junit.QuarkusIntegrationTest
+
+@QuarkusIntegrationTest
+class GreetingResourceIT : GreetingResourceTest()
